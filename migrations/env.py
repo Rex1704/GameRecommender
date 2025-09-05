09ede7f94eef5f00d07067b5645c89bb79a4a7e0
Rex <deepakbaddi8@gmail.com>
2025-09-06 02:19:33 +0530

    corrected some env errors

--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from alembic import context
 from app.extensions import db
 from dotenv import load_dotenv
 
-
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
